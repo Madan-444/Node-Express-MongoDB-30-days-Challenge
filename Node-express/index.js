f24ecd91e3375538d1e3279e7317eed0c419eca5
Madan-444 <mmadan3600@gmail.com>
2020-01-29 22:11:01 +0545

    fourth commit

--- Node-express/index.js
+++ Node-express/index.js
@@ -1,12 +1,49 @@
 const express = require('express')
 const http = require('http');
 const morgan = require('morgan')
+const bodyParser = require('body-parser');
 
 const hostname = 'localhost'
 const port = 8800;
 
 const app = express();
 app.use(morgan('dev'))
+app.use(bodyParser.json());
+
+app.all('/dishes',(req,res,next)=> {
+    res.statusCode=200;
+    res.header('Content-type','text/plain')
+    next();
+})
+
+app.get('/dishes',(req,res,next)=> {
+    res.end('Will send all the dishes to you!')
+})
+app.post('/dishes', (req,res,next)=> {
+    res.end('Will add the dish:' + req.body.name + 'with details:'+ req.body.description)
+})
+app.put('/dishes',(req,res,next)=> {
+    res.statusCode = 403;
+    res.end('Put operation not supported on  /dishes')
+})
+app.delete('/dishes',(req,res,next)=> {
+    res.end('Deleting all the dishes ! ')
+})
+
+app.get('/dishes/:dishId',(req,res,next)=> {
+    res.end('Will send details of'+ req.params.dishId+'to you!');
+})
+app.post('/dishes/:dishId', (req,res,next)=> {
+    res.statusCode=403;
+    res.end('POST operation not supported on /dishes/:dishid' + req.params.dishId);
+})
+app.put('/dishes/:dishId',(req,res,next)=> {
+    res.write('Updating the dish:' + req.params.dishId)
+    res.end('Will update the dish:'+ req.body.name + 'Wtth details:'+ req.body.description)
+})
+app.delete('/dishes/:dishId',(req,res,next)=> {
+    res.end('Deleting all the dishes:' + req.params.dishId)
+})
 
 app.use(express.static(__dirname + '/public'))
 
