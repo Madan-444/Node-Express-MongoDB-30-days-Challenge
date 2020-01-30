4be90228857ac19d23b4bc4016a2eb4319583825
Madan-444 <mmadan3600@gmail.com>
2020-01-30 08:24:28 +0545

    secondNode-express

--- Node-express/index.js
+++ Node-express/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const http = require('http');
 const morgan = require('morgan')
 const bodyParser = require('body-parser');
+const dishRouter = require('./routes/dishRouter')
 
 const hostname = 'localhost'
 const port = 8800;
@@ -10,26 +11,8 @@ const app = express();
 app.use(morgan('dev'))
 app.use(bodyParser.json());
 
-app.all('/dishes',(req,res,next)=> {
-    res.statusCode=200;
-    res.header('Content-type','text/plain')
-    next();
-})
 
-app.get('/dishes',(req,res,next)=> {
-    res.end('Will send all the dishes to you!')
-})
-app.post('/dishes', (req,res,next)=> {
-    res.end('Will add the dish:' + req.body.name + 'with details:'+ req.body.description)
-})
-app.put('/dishes',(req,res,next)=> {
-    res.statusCode = 403;
-    res.end('Put operation not supported on  /dishes')
-})
-app.delete('/dishes',(req,res,next)=> {
-    res.end('Deleting all the dishes ! ')
-})
-
+app.use('/dishes',dishRouter)
 app.get('/dishes/:dishId',(req,res,next)=> {
     res.end('Will send details of'+ req.params.dishId+'to you!');
 })
---
+++ Node-expressTwo/index.js
@@ -0,0 +1,22 @@
+const http = require('http')
+const express = require('express')
+const morgan = require('morgan')
+
+const hostname = 'localhost'
+const port = 5000;
+
+const app = express()
+app.use(morgan('dev'))
+
+app.use(express.static(__dirname + '/public'))
+app.use((req,res,next)=> {
+    console.log(req.headers)
+    res.statusCode = 200;
+    res.setHeader('Content-Type','text/html')
+    res.end('<html><body><h2>This is the Best Express server</h2></body></html>')
+})
+
+const server = http.createServer(app)
+server.listen(port,hostname, ()=> {
+    console.log(`Server running at http://${hostname}:${port}`)
+})
