6c001137c0262521debb658c9e394178d548d9f4
Madan-444 <mmadan3600@gmail.com>
2020-01-31 13:35:19 +0545

    schema update

--- node-mongoose/index.js
+++ node-mongoose/index.js
@@ -7,27 +7,41 @@ const connect = mongoose.connect(url);
 
 connect.then((db)=> {
     console.log('Connected correctly to server');
-    var newDish = Dishes({
-        name: 'Uthappizza',
-        description: 'test'
+    Dishes.create({
+        name: 'madan',
+        description: 'Full stack developer'
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.findByIdAndUpdate(dish._id, {
+            $set: { description: 'Best programmer in the world!'}
+        },{ 
+            new: true 
+        })
+        .exec();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        dish.comments.push({
+            rating: 3,
+            comment: 'I am getting a sinking feeling!',
+            author: 'Leonardo di Carpaccio'
+        })
+
+        return dish.save();
+    })
+    .then((dish) => {
+        console.log(dish);
+
+        return Dishes.remove({});
+    })
+    .then(() => {
+        return mongoose.connection.close();
+    })
+    .catch((err) => {
+        console.log(err);
     });
-
-    newDish.save()
-        .then((dish)=> {
-            console.log(dish);
-
-            return Dishes.find({}).exec();
-        })
-        .then((dishes)=> {
-            console.log(dishes);
-
-            return Dishes.remove({});
-
-        })
-        .then(()=> {
-            return mongoose.connection.close();
-        })
-        .catch((err)=> {
-            console.log(err)
-        });
+    
 });
